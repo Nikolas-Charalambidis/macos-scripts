0ef332b67f739426cf9c790da9a752cd6c1d8519
Nikolas Charalambidis <nikolas.charalambidis@gmail.com>
2023-07-31 13:43:25 +0700

    Update ku-logs.sh

--- ku-logs.sh
+++ ku-logs.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# Define 'help' function
+# Define the 'help' function
 help()
 {
    echo ""
    echo "Usage: $0 -p myPodName -l 1000 -s"
-   echo -e "\t-p MADNATORY: String that Kubernetes pod must contain, for example the pod 'yo-momma-66c4f54568-4fq7k' is found by 'yo-momma'."
+   echo -e "\t-p MANDATORY: String that Kubernetes pod must contain, for example, the pod 'yo-momma-66c4f54568-4fq7k' is found by 'yo-momma'."
    echo -e "\t-l OPTIONAL:  Number of lines if specified, otherwise the full pod logs are downloaded."
    echo -e "\t-s OPTIONAL:  Flag whether the downloaded file should NOT be opened after the script execution, otherwise it is opened by default."
    echo ""
-   echo "Created by Nikolas Charalambidis, see: https://github.com/Nikolas-Charalambidis/macos-scripts"
+   echo "Created by Nikolas Charalambidis, see https://github.com/Nikolas-Charalambidis/macos-scripts"
    echo ""
    exit 1
 }
@@ -21,7 +21,7 @@ do
    	  p ) p="$OPTARG" ;;
       l ) l="$OPTARG" ;;
       s ) s='true' ;;
-      ? ) help ;; # Call 'help' in case parameter is non-existent
+      ? ) help ;; # Call 'help' in case the parameter is non-existent
    esac
 done
 
@@ -45,19 +45,24 @@ DIR=~/Logs/kubectl
 FILE=$DIR/$POD.$NOW.log
 
 touch $FILE
+echo "File..."
+echo " $FILE"
+
 if [[ -z $l ]]; then
-	echo "Extracting all log lines from $POD ..."
+	echo "Extracting all log lines from $POD using the command..."
+	echo " kubectl logs $POD > $FILE"
     kubectl logs $POD > $FILE
 else
-	echo "Extracting last $l log lines from $POD ..."
+	echo "Extracting last $l log lines from $POD using the command..."
+	echo " kubectl --tail=$l logs $POD > $FILE"
     kubectl --tail=$l logs $POD > $FILE
 fi
 
-echo $FILE
-
 if [[ $s != 'true' ]]; then
-	echo "Opening..."
+	echo "Finished, opening..."
 	open $FILE
+else
+	echo "Finished"
 fi 
 
 exit 0
